9af48d6d9f8ac172cc87b6ce9165a3c42d483e44
emrys2015 <33079067+emrys2015@users.noreply.github.com>
2018-03-28 17:59:59 -0700

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -146,7 +146,7 @@ public class Manager {
     public void deleteCourier(String CO_SSN, String CO_DLNO, String DEV_NO){
 
         try{
-            PreparedStatement preparedStatement= con.prepareStatement ("INSERT INTO COURIER VALUES (?,?,?)")
+            PreparedStatement preparedStatement= con.prepareStatement ("INSERT INTO COURIER VALUES (?,?,?)");
             preparedStatement.setInt(1, Integer.parseInt(CO_SSN));
             preparedStatement.setInt(2, Integer.parseInt(CO_DLNO));
             preparedStatement.setInt(3, Integer.parseInt(DEV_NO));
